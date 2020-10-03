ccd2a3eae16fac23231d76abfb14c0434e4191b0
domnixxx <51597030+domnixxx@users.noreply.github.com>
2020-10-03 22:13:01 +0530

    Add files via upload

---
+++ indem.py
@@ -0,0 +1,7 @@
+from gtts import gtts
+import os
+my text=raw_input("Enter the text to be converted")
+language = 'en'
+myonj = gTTS(text=my text, lang=language,  slow=False)
+n=input("enter the name with which it should be saved")
+myobj.save((&n).mp3)
